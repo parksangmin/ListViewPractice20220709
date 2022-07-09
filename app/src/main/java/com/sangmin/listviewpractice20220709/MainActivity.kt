d3c65c5bb6d65642e7322994fccea83941bc441f
박 상 민 <dhwkffks7@naver.com>
2022-07-09 11:25:28 +0900

    3. 리스트뷰의  한칸에 들어갈 데이터 클래스 생성  - 임시적으로 더미데이터 생성

--- app/src/main/java/com/sangmin/listviewpractice20220709/MainActivity.kt
+++ app/src/main/java/com/sangmin/listviewpractice20220709/MainActivity.kt
@@ -4,8 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    val mStudentList = ArrayList<StudentData>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // 학생 데이터 (더미 데이터 생성)
+        mStudentList.add(StudentData("박상민",1995,"서울시 동대문구"))
+        mStudentList.add(StudentData("박상민2",1995,"서울시 동대문구"))
+        mStudentList.add(StudentData("박상민3",1995,"서울시 동대문구"))
+        mStudentList.add(StudentData("박상민4",1995,"서울시 동대문구"))
+        mStudentList.add(StudentData("박상민5",1995,"서울시 동대문구"))
+        mStudentList.add(StudentData("박상민6",1995,"서울시 동대문구"))
+        mStudentList.add(StudentData("박상민7",1995,"서울시 동대문구"))
+        mStudentList.add(StudentData("박상민8",1995,"서울시 동대문구"))
+        mStudentList.add(StudentData("박상민9",1995,"서울시 동대문구"))
+        mStudentList.add(StudentData("박상민10",1995,"서울시 동대문구"))
+
+
+
     }
 }
---
+++ app/src/main/java/com/sangmin/listviewpractice20220709/StudentData.kt
@@ -0,0 +1,12 @@
+package com.sangmin.listviewpractice20220709
+
+class StudentData (
+
+    // 클래스의 맴버 변수
+    val name : String,
+    val birthYear : Int,
+    val address : String
+
+        ){
+
+}
