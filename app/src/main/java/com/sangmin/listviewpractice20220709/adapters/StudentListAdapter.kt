85a5ded8d8ea19ba0dcb701358195c6930a14f9e
박 상 민 <dhwkffks7@naver.com>
2022-07-09 12:21:19 +0900

    4-3 ArrayAdapter가 가지고 있는 getView 함수룰 커스터마이징(override) 해서 우리가 만든 한 칸의 xml

--- app/src/main/java/com/sangmin/listviewpractice20220709/adapters/StudentListAdapter.kt
+++ app/src/main/java/com/sangmin/listviewpractice20220709/adapters/StudentListAdapter.kt
@@ -1,9 +1,11 @@
 package com.sangmin.listviewpractice20220709.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.sangmin.listviewpractice20220709.R
 import com.sangmin.listviewpractice20220709.StudentData
 
 class StudentListAdapter(
@@ -12,6 +14,16 @@ class StudentListAdapter(
     val mList : ArrayList<StudentData>
 
 ) : ArrayAdapter<StudentData>(mContext, resId, mList) {
+    // 한칸의 xml에 나타날 모양을 커스터마이징
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.student_list_item, null)
+        }
+        val row = tempRow!!
+
+        return row
+    }
 
 
 
